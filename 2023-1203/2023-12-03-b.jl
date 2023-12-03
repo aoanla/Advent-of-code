ec896c329800c8ad17fcb51a4cc7e77b3c18c445
aoanla <aoanla@gmail.com>
2023-12-03 12:27:31 +0000

    a bit of tidying of part 2 day 2 soln

--- 2023-1203/2023-12-03-b.jl
+++ 2023-1203/2023-12-03-b.jl
@@ -1,22 +1,20 @@
 
-#part 1
+#part 2
 
-ex_cap(match) = match.captures[1]
+#ranges are *matching ranges* so they're 1 before and 1 after the actual number to get diagonals
+ex_range(match) = (pstart=match.offsets[1]-1, pend = match.offsets[1] + length(match.captures[1]), content=match.captures[1])
 ex_off(match) = (match.offset,UInt[])
 
 function check_symbols(s, pots) #where p is a potential match
     list = UInt[]
     #sindex = 1
-    for p in pots
-        pstart = p.offsets[1] - 1 #because we could be diagonally across from input
-        pend = p.offsets[1] + length(p.captures[1])     
-        if s < pstart || s > pend
+    for p in pots    
+        if s < p.pstart || s > p.pend
             continue;
             #sindex += 1 #move symbol match window so we don't waste time
         end
-        
         #if we're here then it must be in the range!
-        push!(list, parse(UInt, p.captures[1]))
+        push!(list, parse(UInt, p.content))
     end
 
     list
@@ -31,18 +29,18 @@ open("input") do f
     prev_matches = []
     for line in eachline(f) 
         
-        potentials = collect(eachmatch(r"([0-9]+)", line))  #the numbers in this line 
+        potentials = collect(ex_range.(eachmatch(r"([0-9]+)", line)))  #the numbers in this line as ranges and content 
         symbols = Dict(ex_off.(eachmatch(r"([*])", line))) #the locations of the asterisks on this line and their associated numbers
 
         for k in keys(symbols)
-            symbols[k] = check_symbols(k, potentials ∪ old_potentials)
+            symbols[k] = check_symbols(k, potentials ∪ old_potentials)  #check our symbols against candidates on this and above line
         end
 
         for k in keys(old_symbols)
-            append!(old_symbols[k],check_symbols(k, potentials))
+            append!(old_symbols[k],check_symbols(k, potentials)) #check the symbols from the above line against candidates on this line
         end
 
-        for (k,v) in pairs(old_symbols)
+        for (k,v) in pairs(old_symbols)  #"gears" are symbols with two values [this could be a filter but I don't think it matters]
             if length(v) == 2
                 gearvals += v[1]*v[2]
             end
