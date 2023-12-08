e688b910c6d0e80fc07dcdc47e80159afae07807
aoanla <aoanla@gmail.com>
2023-12-08 15:36:47 +0000

    working version - 90% of the issues are winnow being less ergonomic than nom

--- 2023-1208/src/main.rs
+++ 2023-1208/src/main.rs
@@ -10,30 +10,31 @@ use winnow::{
     token::{one_of, take_till},
     ascii::{alphanumeric1, line_ending},
     combinator::{repeat, separated_pair,separated, delimited, terminated, peek},
+    error::InputError,
 };
 use std::collections::HashMap;
 
-
-
 //lets parse efficiently into a Vec in two passes
 pub fn parse_file(input: &mut &str) -> (Vec<usize>, Vec<usize>, Vec<usize>, Vec<[usize;2]>) {
-    let nodenums :HashMap<&str, usize> = HashMap::<&str,usize>::new();
-    let startnums :Vec<usize> = vec![]; //the "A" nodes
-    let endnums :Vec<usize> = vec![]; //the "Z" nodes
+    let mut nodenums :HashMap<&str, usize> = HashMap::<&str,usize>::new();
+    let mut startnums :Vec<usize> = vec![]; //the "A" nodes
+    let mut endnums :Vec<usize> = vec![]; //the "Z" nodes
 
-    let parse_direction = one_of(|c| c=='L' || c=='R').map(|x| if x=='R' {1} else {0} );
-    let directions = terminated(repeat(0..,parse_direction), line_ending).parse_next(input).unwrap();
+    let parse_direction = one_of::<_,_,InputError<_>>(['L','R']).map(|x| if x=='R' {1} else {0} );
+    let directions: Vec<usize> = terminated(repeat(0.., parse_direction), line_ending).parse_next(input).unwrap();
 
-    //let parse_nodename = terminated(alphanumeric1, take_till(0..,|c: char| c.is_ascii_control())); //and discard line
-    //let nodenames: Vec<&str> = peek(separated(1.., parse_nodename, line_ending)).parse_next(input).unwrap();
+    let _ = line_ending::<_, InputError<_>>.parse_next(input);
 
-    let nodes = Vec::<[usize;2]>::new();
-    /* 
+    let parse_nodename = terminated(alphanumeric1::<_, InputError<_>>, take_till(0..,|c: char| c.is_ascii_control())); //and discard line
+    let nodenames: Vec<&str> = peek(separated(1.., parse_nodename, line_ending)).parse_next(input).unwrap();
+
+    let mut nodes = Vec::<[usize;2]>::new();
+
     nodes.resize(nodenames.len(), [0,0]);
 
 
     for (i,n) in nodenames.iter().enumerate() {
-        nodenums[n] = i;
+        nodenums.insert(n,i);
         match *n {
             "AAA" => startnums.push(i), //n[2] == 'A' for part 2
             "ZZZ" => endnums.push(i), //n[2] == 'Z' for part 2
@@ -41,12 +42,14 @@ pub fn parse_file(input: &mut &str) -> (Vec<usize>, Vec<usize>, Vec<usize>, Vec<
         }
     } 
 
-
-    let parse_node_to_num = alphanumeric1.map(|x| nodenums[x]);
-    let parse_node = separated_pair(parse_node_to_num, " = ", delimited('(', separated_pair(parse_node_to_num, ", ", parse_node_to_num) ,')') );
+    //I absolutely should not need to do this... (and I don't think I did with nom...)
+    let parse_node_to_numa = alphanumeric1::<_, InputError<_>>.map(|x| nodenums[x]);
+    let parse_node_to_numb = alphanumeric1::<_, InputError<_>>.map(|x| nodenums[x]);
+    let parse_node_to_numc = alphanumeric1::<_, InputError<_>>.map(|x| nodenums[x]);
+    let parse_node = separated_pair(parse_node_to_numa, " = ", delimited('(', separated_pair(parse_node_to_numb, ", ", parse_node_to_numc) ,')') );
     let parsed_nodes :Vec<(usize, (usize,usize))>= separated(1.., parse_node, line_ending).parse_next(input).unwrap();
     parsed_nodes.iter().for_each(|(id,lr)| nodes[*id] = [lr.0,lr.1] );
-    */
+    
 
     (directions, startnums, endnums, nodes) //for the first one we just need one startnum and one endnum but future expansion!
     
@@ -62,7 +65,7 @@ fn main() {
     //
     let mut num = startnums[0];
     let end = endnums[0];
-    let i = 1;
+    let mut i = 1;
     for n in directions.iter().cycle() {
         num = nodes[num][*n];
         if num == end {
