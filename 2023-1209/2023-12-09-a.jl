340aaf3e434411f28ff461b2ca01e71a27dbbcb5
aoanla <aoanla@gmail.com>
2023-12-09 15:50:36 +0000

    separate parser and solver

--- 2023-1209/2023-12-09-a.jl
+++ 2023-1209/2023-12-09-a.jl
@@ -36,38 +36,67 @@ function finite_diff!(x,l)
     (same,l-1)
 end
 
+"""
+        extend_front_and_back!(x, l)
 
-
-open("input") do f
-    accum = 0;
-    accum2 = 0;
-    for line in eachline(f)
-        samples = append!([0], parse.(Int64,split(line)));
-        l = length(samples);
-        s = false;
-        sgn = 1;
-        next_x = samples[end];
-        prev_x = samples[2];
-        while !s #differentiate until we hit the constant differences
-            #inlined function (s,l) = finite_diff!(samples, l);
-            s = true;
-            samples[1] = samples[3]-samples[2];
-            for i in 3:l
-                samples[i-1]=samples[i]-samples[i-1];
-                s &= (samples[i-1] == samples[i-2]);
-            end
-            l -= 1;
-            #end inline
-            next_x += samples[l];
-            prev_x = samples[2] - prev_x;
-            #(samples,s) = finite_diff(samples);
-            #next_x += samples[end]; #the next element of the top sample, accumulating the leading differences until constant
-            #prev_x = samples[begin] - prev_x; #the previous element of the top sample, reducing "forwards"
-            sgn = -sgn; #our "backwards" reduce above will flip the sign on odd summations so we need to reverse that if needed
+        where x is a vector of values [starting at the 2nd index, the first index being scratch]
+        and l is the index of the last element used for values
+        DESTROYS contents of x
+        returns "zeroth" value of x in x[1] and "n+1th" value of x in x[2]
+"""
+function extend_front_and_back!(x, l)
+    s = false;
+    sgn = 1;
+    next_x = x[end];
+    prev_x = x[2];
+    while !s #differentiate until we hit the constant differences
+        #inlined function (s,l) = finite_diff!(samples, l);
+        s = true;
+        x[1] = x[3]-x[2];
+        for i in 3:l
+            x[i-1]=x[i]-x[i-1];
+            s &= (x[i-1] == x[i-2]);
         end
-        accum += next_x;
-        accum2 += sgn*prev_x; #fix sign!
+        l -= 1;
+        #end inline
+        next_x += x[l];
+        prev_x = x[2] - prev_x;
+        #(samples,s) = finite_diff(samples);
+        #next_x += samples[end]; #the next element of the top sample, accumulating the leading differences until constant
+        #prev_x = samples[begin] - prev_x; #the previous element of the top sample, reducing "forwards"
+        sgn = -sgn; #our "backwards" reduce above will flip the sign on odd summations so we need to reverse that if needed
     end
-    @printf "Part one: %i\n" accum
-    @printf "Part two: %i\n" accum2
+    x[1] = next_x;
+    x[2] = sgn * prev_x;
 end
+
+"""
+        parse_file(fname)
+        
+        where fname is a filename (string)
+
+        returns a *static* array holding the file parsed into 1 column per line
+        assumes the input data is rectangular.
+"""
+function parse_file(fname)
+    open(fname) do f
+        arr = [];
+        for line in eachline(f)
+            push!(arr, append!([0], parse.(Int64,split(line))));
+        end
+        arr
+    end
+end
+
+function solve(fname)
+    arr = parse_file(fname); #parse
+    l = length(arr[1]); #data is rectangular, don't recheck last
+    parts = [0,0];
+    for line in arr
+        extend_front_and_back!(line, l)
+        parts += line[1:2];
+    end
+    parts
+end
+
+println("""$(solve("input"))""");
