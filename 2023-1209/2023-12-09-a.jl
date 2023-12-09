ed802e35f48fccd72f2dbb8709e893f723034512
aoanla <aoanla@gmail.com>
2023-12-09 15:21:33 +0000

    inplace implementation, with function

--- 2023-1209/2023-12-09-a.jl
+++ 2023-1209/2023-12-09-a.jl
@@ -19,19 +19,43 @@ function finite_diff(x)
     (dx[2:end],same)
 end
 
+"""
+    finite_diff!(x, l)
+
+    where x is a vector of values [starting at the 2nd index - the first index being scratch]
+    and l is the position of the *last* element in x with a real value
+    returns new length l, same (if all values of dx are the same), modifies x in place with its differences
+"""
+function finite_diff!(x,l)
+    same = true;
+    x[1] = x[3]-x[2];
+    for i in 3:l
+        x[i-1]=x[i]-x[i-1];
+        same &= (x[i-1] == x[i-2]);
+    end
+    (same,l-1)
+end
+
+
+
 open("input") do f
     accum = 0;
     accum2 = 0;
     for line in eachline(f)
-        samples = parse.(Int64,split(line));
+        samples = append!([0], parse.(Int64,split(line)));
+        l = length(samples);
+        println("$l")
         s = false;
         sgn = 1;
         next_x = samples[end];
-        prev_x = samples[begin];
+        prev_x = samples[2];
         while !s #differentiate until we hit the constant differences
-            (samples,s) = finite_diff(samples);
-            next_x += samples[end]; #the next element of the top sample, accumulating the leading differences until constant
-            prev_x = samples[begin] - prev_x; #the previous element of the top sample, reducing "forwards"
+            (s,l) = finite_diff!(samples, l);
+            next_x += samples[l];
+            prev_x = samples[2] - prev_x;
+            #(samples,s) = finite_diff(samples);
+            #next_x += samples[end]; #the next element of the top sample, accumulating the leading differences until constant
+            #prev_x = samples[begin] - prev_x; #the previous element of the top sample, reducing "forwards"
             sgn = -sgn; #our "backwards" reduce above will flip the sign on odd summations so we need to reverse that if needed
         end
         accum += next_x;
