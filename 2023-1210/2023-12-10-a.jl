6cc0ea1019b0b31c649e601c3df302a46a1ab47f
aoanla <aoanla@gmail.com>
2023-12-10 21:37:09 +0000

    Day 10 part 1 (with "invisible" markers to start off part 2 solution)

--- 2023-1210/2023-12-10-a.jl
+++ 2023-1210/2023-12-10-a.jl
@@ -4,44 +4,102 @@
 
 #this is the first puzzle where we can't really get away with parsing line-by-line and need to pull in the whole file first
 
-data = read("input");
-linedata = data.split
+d = read("input2");
+
+total = length(d); #unsure if I should count the newlines here or not tbh - or if I should floodfill them
+
+b(x) = UInt8(x);
+
+s = findfirst(x-> x==b('\n'), d); #going "up" a row is subtracting stride 
 
 # Tuple(usize,usize)
-n = (-1,0);
-s = (1,0);
-e = (0, -1);
-w = (0, 1);
-START = ((0,0),(0,0));
-NULL = ((-1,-1),(-1,-1)));
+n = -s;
+e = 1;
+w = -1;
+START = 0;
+NULL = 999;
 
-function raw_to_directions(ch)
-    #"julian" switch-case via ternary chain
-    ch == '|' ? (n,s) :
-    ch == '-' ? (e,w) :
-    ch == 'L' ? (n,e) :
-    ch == 'J' ? (n,w) : 
-    ch == '7' ? (s,w) : 
-    ch == 'F' ? (s,e) : 
-    ch == 'S' ? START :
-    NULL ;
-    ch
-end
+decode = Dict([
+     b('|') => (n,s) ,
+     b('-') => (e,w) ,
+     b('L') => (n,e) ,
+     b('J') => (n,w) , 
+     b('7') => (s,w) , 
+     b('F') => (s,e) , 
+     b('S') => (START,START) ])
 
-function find_start_and_connectors(d)
+     #for the S to id it for part 2 leak detection
+encode = Dict([(v=>k) for (k,v) in pairs(decode)]);
+
+
+
+
+function find_start_and_connector(d)
     #find start
-
-    #check four cells around it for the two cells that connect
-    adjs = (c1,c2);
-
-    return (start, adjs)
+    start = findfirst( x -> x==b('S'), d);
+    #check four cells around it for a cell that connects (we don't care which, because we just need a start point)
+    (adj,dir) = d[start+n] in b.(['|', '7', 'F']) ? (start+n, n) :  #items that connect to "their south" == our north
+           d[start+s] in b.(['|', 'L', 'J']) ? (start+s, s) : (start+e, e) ; #only two options left, so it must be both of them!
+           # ch[start+e] in ['-', 'J', '7'] ? start+e : start+w ; #because it must be connected somewhere
+    return (adj,dir)
 end
 
 # indexing map[direction...] splat to map correctly
-function follow_pipe()
-    # d= "direction we just moved in"  
-    newdir = curr_pipe[ (curr_pipe[1]==(.-d))+1  ];  #comparison is 0 or 1, so we can branchlessly map to the end we didn't enter from
-    curr_pipe_loc .+= newdir;
-    curr_pipe = parsed_data[curr_pipe_loc...]; #need to splat the tuple
-
+function follow_pipe(curr_pipe, entry_dir, d)
+    directions = decode[d[curr_pipe]];
+    entry_dir = directions[ (directions[1]==-entry_dir)+1  ];  #comparison is 0 or 1, so we can branchlessly map to the end we didn't enter from
+    curr_pipe += entry_dir;
+    (curr_pipe, entry_dir)
 end
+
+""" modifies d to leave trail """
+function follow_pipe_and_mark!(curr_pipe, entry_dir, d)
+    directions = decode[d[curr_pipe]];
+    entry_dir = directions[ (directions[1]==-entry_dir)+1  ];  #comparison is 0 or 1, so we can branchlessly map to the end we didn't enter from
+    d[curr_pipe] |= 0x80; #set high bit
+    curr_pipe += entry_dir;
+    (curr_pipe, entry_dir)
+end
+
+#=
+"""Floodfill algorithm, bounded by Ss, on d starting at boundaries"""
+function floodfill_with_count(start, d)
+    #fill from top first
+    for i in 1:s  #first row
+        d[i] == b('S')  
+    count
+end
+=#
+
+function solve(d)
+    (curr_pipe, entry_dir) = find_start_and_connector(d);
+    steps = 1;
+    initial_dir = entry_dir; #to id 'S' after
+    while d[curr_pipe] != b('S')
+        (curr_pipe, entry_dir) = follow_pipe(curr_pipe, entry_dir, d);
+        steps += 1;
+    end
+    println("Total $steps to circumnavigate, halfway is thus $(steps ÷ 2)");
+end
+
+solve(d);
+
+#solve part 2, destructively on d
+function solve2!(d)
+    (curr_pipe, entry_dir) = find_start_and_connector(d);
+    start_dir = entry_dir;
+    steps = 1;
+    while d[curr_pipe] != b('S')
+        (curr_pipe, entry_dir) = follow_pipe_and_mark!(curr_pipe, entry_dir, d);
+        steps += 1;
+    end
+    c = encode[(start_dir, -entry_dir)];
+    println("S was a $c !");
+    d[curr_pipe] = c | 0x80; #mark and set type
+    inside = total - steps #"the boundary" can't be inside itself.
+    println("Total $steps to circumnavigate, halfway is thus $(steps ÷ 2)");
+    println("")
+    println("$(String(d.&0x7f))") #remove marker bits
+end
+
+solve2!(d)
