2b10cf3d4d133223c65ab66f60646fcbb1a81580
aoanla <aoanla@gmail.com>
2023-12-12 07:50:39 +0000

    more sketches of recursive approach

--- 2023-1212/2023-1212.jl
+++ 2023-1212/2023-1212.jl
@@ -11,3 +11,16 @@
 # but if that's in the middle, we would also need to check the space left on either side to hold the remaining windows
 
 #obviously, to make the windows easy, we extend each vector of #?. by one . start and end so our window edges fit.
+
+#so, from the left or the right:
+
+#fn(substring)
+# matches = 0;
+#while match(window_n, substring)
+#   if not last window
+#       matches += call fn(window_n+1, substring - [sequence we just consumed])
+#   else
+#       matches+=1
+#return matches
+
+#that seems relatively sensible?
