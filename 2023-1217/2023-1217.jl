c5d629586b70c699a655ba2df7abc27a8bbcfacf
aoanla <aoanla@gmail.com>
2023-12-17 13:11:40 +0000

    working now we're tracking "state" correctly

--- 2023-1217/2023-1217.jl
+++ 2023-1217/2023-1217.jl
@@ -14,6 +14,20 @@ println("$(size(matrix))")
 bounds= size(matrix)
 goal = CartesianIndex(bounds) #that's the last coordinate, so!
 #bounds = bounds(matrix)
+#exp_bounds = vcat([ i for i in bounds ], [4, 3]); #extra dimensions are "direction from" and "amount" 
+
+CI(x,y) = CartesianIndex((x,y))
+
+dir_to_num = Dict([CI(0,1)=>1, CI(0,-1)=>2, CI(1,0)=>3, CI(-1,0)=>4]);
+num_to_dir = [CI(0,1), CI(0,-1), CI(1,0), CI(-1,0)];
+
+
+struct cell_data
+    c::CartesianIndex{2} #the cell itself
+    dir::Int
+    count::Int #its history - how it was got to, and how many successive moves 
+end
+
 
 #note: check this is 1/3 and not 1/4 - the example shows chains of 4 squares in a row from 3 *movements* 
 #           if 1/4 then 3->4 , 5*x÷3 -> (1 + 1/4 + 1/4) = 3*x÷2 ? 
@@ -21,12 +35,12 @@ goal = CartesianIndex(bounds) #that's the last coordinate, so!
 
     Return a suitable heuristic for the remaining distance (encoding the "no more than 3 steps in a straight line" rule)
 """ 
-function h(posn::CartesianIndex{2}, movehist::Tuple{CartesianIndex{2}, UInt8})
+function h(posn::cell_data)
     #improvement - use movehist to tweak this estimate (only really significant for short distances where it matters if we can't move 3 in one dir in one go)
-    return 0 # try with Dijkstra - okay, so the problem isn't h 
-    d = goal - posn
+    #return 0 # try with Dijkstra - okay, so the problem isn't h 
+    d = goal - posn.c;
     #return d[1]+d[2]
-    d == CartesianIndex((0,0)) && return 0
+    d == CI(0,0) && return 0
     (bigger, smaller) = d[1] > d[2] ? (d[1], d[2]) : (d[2], d[1])
     slope = smaller != 0 ? bigger ÷ smaller : bigger ; 
     #we're within the range where we can jink around and still get there within Metropolis distance
@@ -45,11 +59,12 @@ possibles = Set(CartesianIndex.([(0,1), (1,0), (0,-1), (-1,0) ]) );
     - we can't get the node *forward* from movehist if count == 3
     - we can't violate bounds!
 """
-function accessible(c, movehist)
-    notaccessible = movehist[2] == 3 ? [movehist[1], movehist[1]*-1] : [movehist[1]*-1]
+function accessible(c)
+    #                       yes 4, thanks Julia for indexing at 1
+    notaccessible = c.count == 3 ? [num_to_dir[c.dir], num_to_dir[c.dir]*-1] : [num_to_dir[c.dir]*-1]
     p = setdiff(possibles, notaccessible)
     filter(p) do pp 
-        checkbounds(Bool, matrix, c+pp)
+        checkbounds(Bool, matrix, c.c+pp)
     end     
 end
 
@@ -65,20 +80,22 @@ end
 
 function A✴(s::CartesianIndex{2}, g::CartesianIndex{2})
 
-    prev = Dict{CartesianIndex{2}, CartesianIndex{2}}() #dictionary of previous points
+    prev = Dict{cell_data, cell_data}() #dictionary of previous points
+    s_cell = cell_data(s, 1, 1); #count 1 == 0 really (thanks Julia!)
 
-    goalscore = fill(typemax(1), bounds)  #cost s -> cell
-    goalscore[s] = 0 #zero cost to not move at all!
+    goalscore = [ [typemax(1) for i in 1:4, j in 1:4] for k in 1:bounds[1], l in 1:bounds[2] ]  #cost s -> cell
+    goalscore[s][1,1] = 0 #zero cost to not move at all!
 
     #we need to note where we last entered each node from and now many times we'd done that exact direction 
-    movehistory = Dict{CartesianIndex{2}, Tuple{CartesianIndex{2}, UInt8}}([s=>(CartesianIndex(0,0), 0)]);
+    #movehistory = Dict{CartesianIndex{2}, Tuple{CartesianIndex{2}, UInt8}}([s=>(CartesianIndex(0,0), 0)]);
 
-    fscore = fill(typemax(1), bounds) #f, our heuristic estimate for s->g via cell 
-    fscore[s] = h(s, movehistory[s]) #and our best guess for s is just h at the moment 
+    #fscore = fill(typemax(1), bounds) #f, our heuristic estimate for s->g via cell 
+    fscore = [ [typemax(1) for i in 1:4, j in 1:4] for k in 1:bounds[1], l in 1:bounds[2] ] 
+    fscore[s][1,1] = h(s_cell) #and our best guess for s is just h at the moment 
 
 
 
-    openset = PriorityQueue{CartesianIndex{2}, Int}(s => fscore[s] ) #need to sort out *what* we can use as a priority queue in Julia
+    openset = PriorityQueue{cell_data, Int}(s_cell => fscore[s][1,1] ) #need to sort out *what* we can use as a priority queue in Julia
 
     while !isempty(openset)
 
@@ -87,25 +104,26 @@ function A✴(s::CartesianIndex{2}, g::CartesianIndex{2})
         # Julia claims popfirst! is not implemented for PriorityQueue [at least a v0.18.15] and I need to use dequeue! (which is supposed to be deprecate)
         # and only gives K not V !
         cursor = dequeue!(openset) #the highest priority (lowest "value") node
-        score = fscore[cursor]; 
-        cursor == g #=we got there!=# && begin
+        score = fscore[cursor.c][cursor.dir, cursor.count]; 
+        cursor.c == g #=we got there!=# && begin
                                                 println("$(reconstruct_path(prev, cursor))"); 
                                                 return score # the total cost! (I think fscore[cursor] == goalscore[cursor] at this point?)
                                         end 
-        hist = movehistory[cursor]
+        #hist = movehistory[cursor]
         #evaluate neighbours of cursor = which means we need to store the direction we entered cursor from and how long we'd been moving in that direction
-        for i in accessible(cursor, hist)
-            cand = cursor + i;
-            trialgoalscore = goalscore[cursor] + matrix[cand];
-            if trialgoalscore < goalscore[cand]
+        for i in accessible(cursor)
+            di = dir_to_num[i]
+            cand = cell_data(cursor.c+i, di, cursor.dir == di ? cursor.count+1 : 1)
+            trialgoalscore = goalscore[cursor.c][cursor.dir, cursor.count] + matrix[cand.c];
+            if trialgoalscore < goalscore[cand.c][di, cand.count]
                 prev[cand] = cursor 
-                goalscore[cand] = trialgoalscore
-                movehistory[cand] = i == hist[1] ? (i, hist[2]+1) : (i, 1)  #accrue straight lines - it's okay for this to change if cand leaves and re-enters the open set
-                fscore[cand] = trialgoalscore + h(cand, movehistory[cand])
+                goalscore[cand.c][di,cand.count] = trialgoalscore
+                #movehistory[cand] = i == hist[1] ? (i, hist[2]+1) : (i, 1)  #accrue straight lines - it's okay for this to change if cand leaves and re-enters the open set
+                fscore[cand.c][di,cand.count] = trialgoalscore + h(cand)
                 #if haskey[openset] #update priority (which *can* change here I think!)
                 #    openset cand priority = fscore[cand]
                 #else 
-                openset[cand] = fscore[cand]
+                openset[cand] = fscore[cand.c][di,cand.count]
                 #end
             end
         end
