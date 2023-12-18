7bfc300814ac3178ac722d4c417232610691beca
aoanla <aoanla@gmail.com>
2023-12-18 17:56:55 +0000

    Hm, I was sure this would be right, but it's still *slightly* off. I hate pixels.

--- 2023-1218/2023-1218-b.jl
+++ 2023-1218/2023-1218-b.jl
@@ -32,26 +32,88 @@ function safepush!(dict, key, value)
 end
 
 function read_instructions(f)
-    instructions = []
     rowcol = [1,1]
+    accum = 0
     open(f) do fd
+        last_col_step = 0; #we'll need to fix the "overlap on the last -> first"  at the end
+        first = true
+        first_col_step = 0
+        last_row_step = 0;
+        last_row_size = 0;
+        first_row = 1 #first row position actually *is* 1
+        last_row = 1 #fixed up by loop
         for l in readlines(fd)
             _, _, colour = split(l, ' ');
             dir = colour[end-1];
             l = parse(Int, "0x" * colour[begin+2:end-2])
-            println("$dir $l $colour")
+            #println("$dir $l $colour")
             #0 R 1 D 2 L 3 U
-            select = ( dir == '2' || dir == '0' ) ? COL : ROW   #COL now is "COLranges" stored in ROW hash
-            nselect = 3 - select; #2 if 1, 1 if 2 
+            select = ( dir == '2' || dir == '0' )    #COL now is "COLranges" stored in ROW hash
+            nselect = !select; #2 if 1, 1 if 2 
             step = ( dir == '3' || dir == '2' ) ? -1 : 1
+            if first && select
+                first_col_step = step #grab this for sorting out at the end
+                first = false
+            end
+#we need an ordered list of oriented rects to sort out the double counting at borders where both rects have same sign, so just do it here
 
-            new = rowcol[select] + step*l
-            safepush!(rowscols[nselect], rowcol[nselect], (rowcol[select], new) ) 
-            rowcol[select] = new
+
+            #if this is going to ROW, then add its signed area
+            #println("$select")
+            if select == true                              #0 if step positive, -1 if negative  # width of range *exclusive of end point (at far end)*
+                    accum -= (rowcol[nselect+1] -           (1+step)÷2)                *   (step*(l)) #difference, removing off-by-one
+                    #it's actually problematic to have inclusive ranges because correcting for overlap is super hard for positive segments 
+                    if step == last_col_step #if we are adjacent to another span with same sign, correct the overlap
+                        println("$step == $last_col_step , rowstep is $last_row_step")
+                        #if step is +ve  and row_step is +ve then we've oversubtracted by row_size
+                        if step > 0 && last_row_step > 0 
+                        #println("overlap $(last_row_step * step)")
+                            println("1")
+                            accum -= last_row_size;
+                            #if step is -ve and row_step is -ve then all is well
+                            #if step is +ve and row_step is +ve then all is well 
+                            #if step is -ve and row_step is -ve then we undercounted by row_size
+                        elseif step < 0 && last_row_step < 0
+                            println("2")
+                            accum += last_row_size
+                        end
+                    else #step and last step are different
+                        #curr step is -ve (last step was +ve) and row_step is +ve then we've over counted by (rowcol[nselect+1] - last_row_size)!
+                        if step < 0 && last_row_step > 0
+                            println("3")
+
+                            accum -= +last_row_size #(rowcol[nselect+1]- last_row_size )
+                            #curr step is +ve (last step was -ve) and row_step is -ve then, we've undercounted by last_row_size ?
+                        elseif step > 0 && last_row_step < 0
+                            println("4")
+
+                            accum += last_row_size  #?
+                        end
+                        #curr step is -ve (last step was +ve) and row_step is -ve then ... is this okay? Is this possible if we wind anticlockwise?
+                        #curr step is +ve (last step was -ve) and row_step is +ve then ... is this okay? Is this possible if we wind anticlockwise?
+
+                        #if last_col_step != last_row_step #winding involution causes us to miscount a row here
+                        #println("Winding involution")
+                        #accum += last_row_size - 1 ; 
+                    end
+                    last_col_step = step  
+            else #ROW 
+                #println("ROW")
+                last_row_step = step;
+                last_row_size = l;
+            end
+            #safepush!(rowscols[nselect], rowcol[nselect], (rowcol[select], new) ) 
+            rowcol[select+1] += step*l
         end
-    end
+            #fixup the last col span loop to the first 
+        println("$(last_row_step)");
+        #for our data, this doesn't trigger
+        if first_col_step == last_col_step 
 
-    #raycasting, lets just pick rows to do this over
+            accum += 1 - rowcol[1]
+        end 
+        #accum += last_row_size;
+    end
 
     #PT2 note - this is now going to be raycasting through ranges.
     #trick will be:
@@ -61,29 +123,12 @@ function read_instructions(f)
 
     #... this is easier than that because you can just ignore the colranges by row [as the rowranges by cols all have them as limits]
     # in which case this is just adding oriented areas of rectangles from each range
-    min_k = minimum(collect(keys(rowscols[ROW])));
-    insidespace = 0
-    error = 0
-    for (k,v) in rowscols[ROW]
-        for vv in collect(unique(v))
-            dist = vv[1]-vv[2]
-            oriented_area = (k ) * (dist + 1) # + sign(dist)); #pick a direction as positive, I think we go down first on the outside so...
-            error += (k) * sign(vv[1]-vv[2]) 
-            insidespace += oriented_area;
-        end
-    end
-    rowsacc =0
-    for (k, v) in rowscols[COL]
-        for vv in collect(unique(v))
-            rowsacc += (abs(vv[1]-vv[2]) +1) 
-        end
-    end
-    println("$rowsacc")
-    insidespace - 952408144115
+
+    accum 
 
 
     
 end
 
 println("$(read_instructions("input2"))")
-
+println("952408144115")
