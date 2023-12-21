f8c9bf117a3ffe8186cc65d3cab37c93a7183dd1
aoanla <aoanla@gmail.com>
2023-12-21 09:07:06 +0000

    thoughtson Day 21

---
+++ 2023-1221/2023-1221.jl
@@ -0,0 +1,9 @@
+#This feels like the secret is to pop things off the "accessible" queue once you get a loop to them that's a power of 2 (since that means it will
+#recur @ 64)
+
+#so, you want a "nodes visited" dict with a "seen at" value 
+#and a "nodes we're currently at" Set which we expand from each time
+#and a "nodes with a period of 2^n" Set you push things into when you meet them again at the right time?
+
+#with a rule that when you do the next "step update", you *don't* move to any nodes in the "period 2^n" list
+# (which breaks those cycles and means that we don't grow an exponential number of nodes we're tracking at time t since we're pruning short cycles as we go)
