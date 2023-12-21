25f96c5bedbb3492d628e94c3e3e96158ebbf2f0
aoanla <aoanla@gmail.com>
2023-12-21 18:09:20 +0000

    A note about Day 21 pt 2

--- 2023-1221/2023-1221.jl
+++ 2023-1221/2023-1221.jl
@@ -12,3 +12,46 @@
 #Further thought before going out to do other things: no, this is slightly more subtle than that: *all* points where we can reach them at an even timestep are in the set
 # (because we can then trivially step off them and back into them endlessly until we get any other even number)
 # - are there any odd points which are also in the set? anything we can orbit with an odd cycle ?
+
+six_four_nodes = Set{CartesianIndex{2}}()
+
+map_raw = read("input2")
+width = findfirst(==(UInt8('\n')), map_raw);
+matrix = (reshape(map_raw, width, :)[begin:end-1, :]);
+
+starter = findfirst(==(UInt8('S')), matrix)
+println("$matrix")
+
+println("$(UInt8('S'))")
+
+N = CartesianIndex(-1, 0)
+S = CartesianIndex(1, 0)
+E = CartesianIndex(0, 1)
+W = CartesianIndex(0, -1)
+
+function expand(actives, matrix)
+    new_actives = Set{CartesianIndex{2}}()
+    println("$actives")
+    for cell ∈ actives
+         println("$cell")
+         for dir ∈ [N, E, S, W]
+            cand = cell + dir
+            #   must be in bounds               and not an already found 64     and not going into a wall  
+            checkbounds(Bool, matrix, cand) && !(cand ∈ six_four_nodes) && matrix[cand] != UInt8('#') && push!(new_actives, cand);
+         end
+    end
+    new_actives
+end
+
+active_nodes = Set{CartesianIndex{2}}([starter])
+for count ∈ 1:10
+    global active_nodes = expand(active_nodes, matrix)
+    if count % 2 == 0
+        union!(six_four_nodes, active_nodes)
+    end
+end
+
+println("$(length(six_four_nodes))");
+
+#part 2
+#annoyingly 26501365 doesn't factor nicely (it's 5 x 11 x 481843)
