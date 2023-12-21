d76a260b08b0e0103b621c670ada00e812fd7503
aoanla <aoanla@gmail.com>
2023-12-21 10:39:15 +0000

    more thought on Day 21 maybe overcomplicating

--- 2023-1221/2023-1221.jl
+++ 2023-1221/2023-1221.jl
@@ -7,3 +7,8 @@
 
 #with a rule that when you do the next "step update", you *don't* move to any nodes in the "period 2^n" list
 # (which breaks those cycles and means that we don't grow an exponential number of nodes we're tracking at time t since we're pruning short cycles as we go)
+
+
+#Further thought before going out to do other things: no, this is slightly more subtle than that: *all* points where we can reach them at an even timestep are in the set
+# (because we can then trivially step off them and back into them endlessly until we get any other even number)
+# - are there any odd points which are also in the set? anything we can orbit with an odd cycle ?
