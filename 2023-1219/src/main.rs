2bc950504922f7f7ff3cb2189470cdb10f4f134a
aoanla <aoanla@gmail.com>
2023-12-23 23:09:06 +0000

    working through Day19 cargo complaints slowly

--- 2023-1219/src/main.rs
+++ 2023-1219/src/main.rs
@@ -5,7 +5,7 @@
 */
 use once_cell::sync::Lazy; //for "static Regexes"
 use regex::Regex;
-use std::collections::HashMap;
+use std::collections::{ HashSet, HashMap } ;
 use std::fs::read_to_string;
 
 // Stuff we need for range
@@ -23,6 +23,7 @@ struct Interval {
 }
 
 // Stuff we need for tree
+#[derive(Copy, Clone, PartialEq)]
 enum Item {
     X,
     M,
@@ -34,45 +35,51 @@ enum Item {
 
 
 impl Interval {
-    fn gt(&self, lim: i16) -> (Some<Interval>, Some<Interval>){
+    fn gt(&self, lim: i16) -> (Option<Interval>, Option<Interval>){
         if self.h <= lim { //all of the range is below lim, so none goes left
             (None, Some(self))
         } else if self.l > lim { //all of the range is above lim, so none goes right
             (Some(self),None)
         } else {
-            (Some(Interval(lim+1,self.h)), Some(Interval(self.l,lim)))
+            (Some(Interval{l:lim+1,h:self.h}), Some(Interval{l:self.l,h:lim}))
         }
     }
 
-    fn lt(&self, lim: i16) -> (Some<Interval>, Some<Interval>){
+    fn lt(&self, lim: i16) -> (Option<Interval>, Option<Interval>){
         if self.l >= lim { //all of the range is above lim, so none goes left
             (None, Some(self))
         } else if self.h < lim { //all of the range is below lim, so none goes right
             (Some(self),None)
         } else {
-            (Some(Interval(self.l,lim-1)), Some(Interval(lim,self.h)))
+            (Some(Interval{l:self.l, h:lim-1}), Some(Interval{l:lim,h:self.h}))
         }
     }
 }
 
 
-type XMASRange [Interval ; 4] //X M A S
+#[derive(Eq, PartialEq)]
+type XMASRange = [Interval;4]; //X M A S
 
+trait XMAS {
+    fn sel(&self, index: Item, selector: Item, val: Interval) -> Interval;
+    fn one_diff_range(&self, selector: Item, val: Interval ) -> Self ;
+    fn gt(&self, lim: i16, selector: Item ) -> (Option<Self>, Option<Self>) where Self: Sized;
+    fn lt(&self, lim: i16, selector: Item ) -> (Option<Self>, Option<Self>) where Self: Sized;
+}
 
-
-impl XMASRange {
+impl XMAS for XMASRange {
     //Selector type for easily expressing "replace just one element of this array in the new array"
     fn sel(&self, index: Item, selector: Item, val: Interval) -> Interval {
-        selector == index && return val 
-        return self[index]
+        selector == index && return val ;
+        return self[index as usize]
     }
 
-    fn one_diff_range(&self, selector: Item, val: Interval ) -> XMASRange {
-        [self.sel(X as usize, selector, val), self.sel(M as usize, selector, val), self.sel(A as usize, selector, val), self.sel(S as usize, selector, val)]
+    fn one_diff_range(&self, selector: Item, val: Interval ) -> Self {
+        [self.sel(Item::X, selector, val), self.sel(Item::M, selector, val), self.sel(Item::A, selector, val), self.sel(Item::S, selector, val)]
     }
 
 
-    fn gt(&self, lim: i16, selector: Item) -> (Some<XMASRange>, Some<XMASRange>) {
+    fn gt(&self, lim: i16, selector: Item) -> (Option<XMASRange>, Option<XMASRange>) {
         //item select logic here  - or could just use map_or(None, XMASRange(.... r.....)) instead
         match self[selector as usize].gt(lim) {
             (None, None)    => (None, None), //this should never happen!
@@ -84,7 +91,7 @@ impl XMASRange {
     }
 
     //and the same for lt here
-    fn lt(&self, lim: i16, selector: Item) -> (Some<XMASRange>, Some<XMASRange>) {
+    fn lt(&self, lim: i16, selector: Item) -> (Option<XMASRange>, Option<XMASRange>) {
         //item select logic here  - or could just use map_or(None, XMASRange(.... r.....)) instead
         match self[selector as usize].lt(lim) {
             (None, None)    => (None, None), //this should never happen!
@@ -98,15 +105,15 @@ impl XMASRange {
 }
 
 
-
+#[derive(Copy, Clone, PartialEq)]
 enum Cmp{
     GT,
     LT, 
 }
 
 enum Node {
-    Accept{state: Option<XMASRange>},
-    Reject{state: Option<XMASRange>},
+    Accept(Option<XMASRange>),
+    Reject(Option<XMASRange>),
     // "state" here is the state of the Range when entering this node so we can do a cheaper DFS
     Split{state: Option<XMASRange>, item: Item, cmp: Cmp, val: i16, left: Box<Node>, right: Box<Node>},
 }
@@ -115,13 +122,13 @@ enum Node {
 impl Node {
     fn process(&mut self, queue: &mut Vec<XMASRange>, acceptlist: &mut HashSet<XMASRange>)  {
         match self {
-            Accept(s) => acceptlist.insert(s)   ,/*push to Accept list */
-            Reject(_) => {},  /* don't do anything */
+            Node::Accept(s) => acceptlist.insert(s)   ,/*push to Accept list */
+            Node::Reject(_) => {},  /* don't do anything */
             n => {
-                    (L, R) = match self.cmp {
-                                GT =>  n.state.gt(n.val, n.item)
-                                LT =>  n.state.lt(n.val, n.item)
-                    }
+                    let (L, R) = match self.cmp {
+                                Cmp::GT =>  n.state.gt(n.val, n.item),
+                                Cmp::LT =>  n.state.lt(n.val, n.item),
+                    };
                     //*if node gets Some(state), bother to push it to the queue - L should be on the *top* of the queue for DFS */
                     if let Some(right_v) = R {self.right.state = right_v; queue.push(&mut self.right)};
                     if let Some(left_v) = L {self.left.state = left_v; queue.push(&mut self.left)};
@@ -133,52 +140,52 @@ impl Node {
 }
 
 fn recursive_parse(start: &str, lookup: &HashMap<&str, &str>) -> Box<Node> {
-    parse_str = lookup[start];
+    let parse_str = lookup[start];
     recursive_anon(&parse_str, &lookup)
 }
 
 /* process input into tree */
 fn recursive_anon(parse_str: &str, lookup: &HashMap<&str, &str>) -> Box<Node> {
-    static re Lazy<Regex> = Lazy::new(|| regex.Regex::new(r"([xmas])([><])([0-9]+):([^,]),(.*)$").unwrap() );
+    static re: Lazy<Regex> = Lazy::new(|| Regex::new(r"([xmas])([><])([0-9]+):([^,]),(.*)$").unwrap() );
     let Some(captures) = re.captures(parse_str);
     //begin parsing
     let item = match &captures[1] {
-        'x' => X, 
-        'm' => M,
-        'a' => A,
-        's' => S, 
+        "x" => Item::X, 
+        "m" => Item::M,
+        "a" => Item::A,
+        "s" => Item::S, 
     };
     let cmp = match &captures[2] {
-        '>' => GT, 
-        '<' => LT,
-    }
-    let val = &captures[3].parse::<i16>().unwrap() //parse digits as i16
+        ">" => Cmp::GT, 
+        "<" => Cmp::LT,
+    };
+    let val = &captures[3].parse::<i16>().unwrap(); //parse digits as i16
     //these need regexes so we can get the whole string & also check "x" versus "xjajaj"
     let left = match &captures[4] {
-        "A"     => Accept(None),
-        "R"     => Reject(None),
+        "A"     => Box::new(Node::Accept(None)),
+        "R"     => Box::new(Node::Reject(None)),
         n       => recursive_parse(n, &lookup),
-    }   
+    };
     //gnarly splitting on if there's a } or a <> in the second position of this capture 
     // either [xmas]<>... (in which case recursively expand)
     // or  [AR]} in which case we're at the end
     // or glarb} in which case we go to node glarb
-    let right = match &captures[5][1] {
-        '}' =>  match &captures[5][0] {
-            'A' => Accept(None),
-            'R' => Reject(None),
+    let right = match &captures[5][1 as usize] {
+        '}' =>  match &captures[5][0 as usize] {
+            'A' => Box::new(Node::Accept(None)),
+            'R' => Box::new(Node::Reject(None)),
             },
         '<' | '>' => recursive_anon(&captures[5], &lookup), //make anonymous nodes for intermediate bits from the substring starting with this letter
         n => recursive_parse(&captures[5][:len(&captures[5])-1], &lookup),
-    }
+    };
 
-    Box::new(Node { 
+    Box::new(Node::Split{ 
         item: item, 
         cmp: cmp,
         left: left, 
         right: right, 
-        val: val,
-        status: None, 
+        val: *val,
+        state: None, 
     })
 }
 
@@ -191,7 +198,7 @@ fn recursive_anon(parse_str: &str, lookup: &HashMap<&str, &str>) -> Box<Node> {
 // add our item we started with 
 //
 fn parse(s: &str) -> Box<Node> {
-    let buffer = fs::read_to_string(s).unwrap(); 
+    let buffer = std::fs::read_to_string(s).unwrap(); 
 
   /* stuff to remove the "null operations" that branch to the same thing (A or R) on both sides
         //just did this in bash, it was easier
@@ -210,13 +217,13 @@ fn parse(s: &str) -> Box<Node> {
     let temp_dict = HashMap::<&str, &str>::new();
     //stick our name -> { } stuff into this for easier lookup
     for line in buffer.readlines() {
-        key, val = line.split("{") ;
-        temp_dict[key] = val
+        let (key, val) = line.split("{") ;
+        temp_dict[key] = val;
     }
 
     //I feel like we should sort this input somehow to make making the tree easier
     //maybe we at least ensure we start with "in" as the first node?
-    in_node = recursive_parse("in", &temp_dict)
+    let in_node = recursive_parse("in", &temp_dict);
     //and return our in node (along with the rest of the tree?)
     in_node 
 }
@@ -227,9 +234,9 @@ fn parse(s: &str) -> Box<Node> {
 /* process the tree, DFS */
 fn get_ranges(in_node: &mut Node) -> HashSet<XMASRange> {
     /* then walk the tree */
-    in_node.state = XMASRange(Interval(1,4000), Interval(1,4000), Interval(1,4000), Interval(1,4000));
+    in_node.state = [Interval{l: 1, h: 4000}, Interval{l:1, h:4000}, Interval{l:1, h:4000}, Interval{l:1, h:4000}];
 
-    let accepts = HashSet<XMASRange>::new();
+    let accepts = HashSet::<XMASRange>::new();
     let queue = Vec::<XMASRange>::new();
     queue.push(in_node);
     let mut node_now = queue.pop();
@@ -241,18 +248,18 @@ fn get_ranges(in_node: &mut Node) -> HashSet<XMASRange> {
 }
 
 //do Range coalescence stuff for 4d overlapping ranges (bleh)
-fn distinct_ranges(xmas_set: &mut HashSet<XMASRange>) -> i64 {
+fn distinct_ranges(xmas_set: &HashSet<XMASRange>) -> i64 {
     //urgh, of course we don't need to worry about interval intersections - this is a *tree* not a general graph, the intervals can't not be distinct!
 
     //and sum to get answer
-    xmas_set.iter().fold(0, |acc, item| acc + item.iter().product())
+    xmas_set.iter().fold(0, |acc, item| acc + item.iter().product::<i64>()) //need to impl the internal product here differently
 }
 
 
 fn main() {
 
-    let tree_node = parse("input_sanitized");
-    let intervals = get_ranges(&tree_node);
+    let mut tree_node = parse("input_sanitized");
+    let intervals = get_ranges(&mut tree_node);
     let answer = distinct_ranges(&intervals);
     println!("{answer}");
 }
