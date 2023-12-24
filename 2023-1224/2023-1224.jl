00d05fa03f7f2906892f451bc3d1fc9cda4f3c27
aoanla <aoanla@gmail.com>
2023-12-24 12:27:09 +0000

    Day 24 pt 1 with Int128s!

--- 2023-1224/2023-1224.jl
+++ 2023-1224/2023-1224.jl
@@ -13,19 +13,23 @@
 # (and x via either starting equation)
 # check either x or y in the range we consider, and done
 
+#to check time we can just check time for x 
+# x = A + Dt -> t = (x-A)/D = (1/D)x - A/D = γx - δ
+
 struct Hail
     α::Rational
-    β::Rational 
+    β::Rational
+    γ::Rational 
+    δ::Rational
     raw::Tuple{Int, Int, Int, Int, Int, Int}
 end 
 
-hails = open("input2") do f
+hails = open("input") do f
     hails = Hail[]
     re = r"(-?[0-9]+),\s+(-?[0-9]+),\s+(-?[0-9]+)\s+@\s+(-?[0-9]+),\s+(-?[0-9]+),\s+(-?[0-9]+)"
     for line in readlines(f)
-        println("$line")
-        A,B,C,D,E,F = parse.(Int, match(re, line).captures)
-        push!(hails, Hail(A-(D*B//E), D//E, (A,B,C,D,E,F)) )
+        A,B,C,D,E,F = parse.(Int128, match(re, line).captures) #wow we need 128 bit integers!
+        push!(hails, Hail(A-(D*B//E), D//E, 1//D, A//D, (A,B,C,D,E,F)) )
     end 
     hails
 end
@@ -40,17 +44,23 @@ end
 
 function intersect_range(hails, minr, maxr)
     counter = 0
+    hails = filter(hails) do h #positive time hail
+        !( (h.raw[1] < minr && h.raw[4] < 0) || (h.raw[1] > maxr && h.raw[4] > 0) || (h.raw[2] < minr && h.raw[5] < 0) || (h.raw[2] > maxr && h.raw[5] > 0) )
+        #starts with x less than r and going down || starts with x gt than r and going up ; same for y  
+        #we still have some segments which *start* in the interval and leave it, or enter the interval midway
+        # so we still need to check intersection time, but hopefully this winnows out the impossible cases 
+    end
     for i in eachindex(hails), j in eachindex(hails)[i+1:end]
             res = intersect(hails[i], hails[j])
             isnothing(res) && continue 
             x,y = res
-            #println("Intersection of $(hails[1]) and $(hails[2]) @ $x,$y")
+            (hails[i].γ*x - hails[i].δ <= 0 || hails[j].γ*x - hails[j].δ <= 0 ) && continue #past for one or both 
             (x < minr || x > maxr || y < minr || y > maxr) && continue 
-            println("Intersection of $(hails[i]) and $(hails[j]) @ $x,$y")
+            #println("Intersection of $(hails[i]) and $(hails[j]) @ $x,$y ")
             #also need to check if t is negative!
             counter += 1
     end
     counter 
 end
 
-println("$(intersect_range(hails, 7, 27))")
+println("$(intersect_range(hails,  200000000000000, 400000000000000))")
