02315a30560c93e41f255e659a78a6a7f114be40
aoanla <aoanla@gmail.com>
2023-12-25 08:25:13 +0000

    Notes on Day 25

---
+++ 2023-1225/2023-1225.jl
@@ -0,0 +1,5 @@
+#this is the minimum-cut problem, with the advantage that we are given the size of the cut (3)
+# which is often the hardest part (determining if the cut is minimal)
+
+# IIRC, this is Karger's algorithm - which is probabalistic but since we *know* the cut size we can make it deterministic by repeating until k = 3
+
