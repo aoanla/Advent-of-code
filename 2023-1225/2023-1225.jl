c4bbdde7ce2a7af541bb532081395690de2d4bbb
aoanla <aoanla@gmail.com>
2023-12-25 23:26:48 +0000

    I think this is right, but it's a bit slow (I did it the naive way)

--- 2023-1225/2023-1225.jl
+++ 2023-1225/2023-1225.jl
@@ -7,8 +7,8 @@
 
 NODE = String
 
-node_list, adj_matrix = open("input2") do f
-    ns = NODE[]
+node_list, adj_matrix = open("input") do f
+    ns = Set{NODE}()
     adj_matrix = Dict{NODE, Set{NODE}}()
     for line ∈ readlines(f)
         node, nodes = split(line, ": ")
@@ -31,30 +31,63 @@ node_list, adj_matrix = open("input2") do f
     (ns, adj_matrix)
 end
 
-#EDGE = Set{NODE}
-size = length(ns)
+edges(a_m) = collect(Iterators.flatten(map(collect(pairs(a_m))) do p
+    k,v=p
+    [(k,vv) for vv ∈ v]
+end ) )
 
-#min_cut_phase 
-function min_cut_phase(ns, adj_matrix)
-    A = a node in ns
-    if length(A) != size
-        #find most densely connected vertex to nodes in A 
-        for nn in setdiff(ns, A) #nodes not in A 
-            weight = sum( adj_matrix[nn]     )
+#Karger's algorithm
 
-function stoer_wagner(ns, adj_matrix)
-    ns_ = deepcopy(ns)
-    adj_matrix_ = deepcopy(adj_matrix)
-    edges_ = deepcopy(edges)
+function contract(nodes, a_m)
+    ns = deepcopy(nodes)
+    am = deepcopy(a_m)
 
+    while length(ns) > 2
+        k = rand(edges(am))
+        k_n = k[1] * k[2]
+        #remove k from ns, am by contraction
+        pop!(ns, k[1])
+        pop!(ns, k[2])
+        push!(ns, k_n)
+        k_nlist =  union(am[k[1]], am[k[2]])
+        pop!(k_nlist, k[1])
+        pop!(k_nlist, k[2])
+        pop!(am, k[1])
+        pop!(am, k[2])
+        for kk ∈ k_nlist
+            if k[1] ∈ am[kk]
+                pop!(am[kk],k[1])
+            end
+            if k[2] ∈ am[kk]
+                pop!(am[kk], k[2])
+            end
+            push!(am[kk], k_n)
+        end
+        am[k_n] = k_nlist
+    end 
+    setA,setB = values(ns)
+    #println("$setA $setB")
+    nodes_a = String.(collect(Iterators.partition(setA, 3)))
+    nodes_b = Set(String.(collect(Iterators.partition(setB, 3))))
+    cuts = []
+    len = 0
+    #println("$(keys(a_m))")
+    for node ∈ nodes_a
+        nodebs = (a_m[node] ∩ nodes_b)
+        len += length(nodebs)
+        #for nb ∈ nodebs
+        #    push!(cuts,(node, nb))
+        #end
+    end
+    res = length(setA)*length(setB) // 9
 
-    
+   (len, res)     
 end
 
 n = 99
-cuts = Set();
-#while n != 3
-    println("Trying a cut!")
-    global n, cuts = contract(node_list, adj_matrix,edges)
-#end 
-print("$cuts")
+res = 0;
+while n != 3
+    #println("Trying a cut!")
+    global n, res = contract(node_list, adj_matrix)
+end 
+print("$res")
