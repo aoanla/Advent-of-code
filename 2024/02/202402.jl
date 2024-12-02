9478db239825e76a50d6e806890a691709a28f54
aoanla <aoanla@gmail.com>
2024-12-02 07:53:08 +0000

    Julia 2024 02 Pt2

--- 2024/02/202402.jl
+++ 2024/02/202402.jl
@@ -1,12 +1,9 @@
 using DelimitedFiles
 
-input = readlines("input");
-
-print("$(input[1,:])")
+input = readlines("inputtest");
 
 
-pt1 = map(input) do line 
-    l = parse.(Int64, split(line, " "))  
+function validate(l)
     (oldli,nxt) = iterate(l)
     (li, nxt) = iterate(l,nxt)
     olddiff = li - oldli
@@ -23,8 +20,32 @@ pt1 = map(input) do line
         li = newli
     end
     true
+end
+
+pt1 = map(input) do line 
+    l = parse.(Int64, split(line, " "))
+    validate(l)
 end |> sum
 
 #pt1 = sum(mapslices(test_line, input; dims=[2,]))
 
-print("$pt1")
+print("Pt1: $pt1\n")
+
+#pt 2, the same, but we iterate over versions of the list skipping an element
+#and result is true if any of them are true (as soon as any are true)
+
+pt2 = map(input) do line 
+    l = parse.(Int64, split(line, " "))
+    if validate(l) 
+        true
+    else
+        for i ∈ eachindex(l)
+            if validate(vcat(l[begin:i-1],l[i+1:end]))
+                return true
+            end
+        end
+        false
+    end
+end |> sum
+
+print("Pt2: $pt2\n")
