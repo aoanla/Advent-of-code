9d98cf7f7223fb670194c95e20db03a0e88307ea
aoanla <aoanla@gmail.com>
2024-12-06 12:12:08 +0000

    Julia 2024 06 pt2 clarify intent

--- 2024/06/202406.jl
+++ 2024/06/202406.jl
@@ -76,4 +76,4 @@ print("$(pt1(s,loc))")
 #conditions - presumably can't be "on the initial line of the guard", not just where she is 
 # for each new line, new possible obstructions only possible if there's an intersection (?)
 # and we would fold back onto the previous path (that is, we intersect with a right-turn compatible line)
-# -- or we *would* join a right-turn compatible line if we turned right
+# -- or we *would* join a right-turn compatible line if we turned right [special case for the initial position, where we start "mid line" so points "before" it also count]
