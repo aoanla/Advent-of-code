7777bd4159e283d5fa0f771aae00cd9e6b00714a
aoanla <aoanla@gmail.com>
2024-12-07 18:25:47 +0000

    Julia 2024 07 pt2 "backward" wip

---
+++ 2024/07/202407_backward.jl
@@ -0,0 +1,103 @@
+## other approaches: we could try testing divisibility of the solutions from the right hand side (and "deconcating" them)
+###  I'm not sure of the time trade off between division being slow and this easily discounting parts of the search space (it might pay off, but...)
+
+### lets try the above
+
+struct item
+    target::Int64
+    elems::Vector{Int64}
+    memo::Vector{Int64}
+#    revmemo::Vector{Int64}
+end
+
+probs = Set(map(readlines("inputtest")) do l
+    s,p = split(l,':')
+    p = parse.(Int128,collect(split.(p)))
+    memo = reverse(cumsum(p)) #making use of commutivity of + = these are the sums for the *rhs* fragments, for a given position 
+    item(parse(Int128,s),reverse(p), [memo; 0])
+end)
+
+print("$probs")
+
+function try_asterisk(i)
+    max_ = length(i.elems) - 1
+    target = i.target
+    for p ∈ 1:max_
+        divisor = i.elems[p]
+        target % divisor != 0 && return false #asterisk not possible here for all substrings as target is not divisible
+        target_ask = i.target ÷ divisor
+        target_ask == i.memo[p+1] && return true
+        #and recurse 
+        try_asterisk( item(target_ask, i.elems[p+1:end], i.memo[p+1:end]) ) && return true
+
+        # we *loop* by subtracting the next element from p!!!
+        target -= divisor
+    end
+    return false
+end
+
+valid = Set{item}()
+for i ∈ probs
+    #i.target < first(i.memo) && print("sum case: $i\n") #values that are just 1 in the sum *do* reduce the total when multiplied so we can't early exit here
+    if i.target == first(i.memo) #possible that we might have a solution for all sums
+        push!(valid, i)
+        continue
+    end 
+    try_asterisk(i) && push!(valid, i)
+end
+
+pt1 = mapreduce(x->x.target, +, valid)
+
+print("Pt1 = $(pt1)\n")
+
+# Pt2 
+
+function deconcat_p(x,y)
+    places = floor(Int64,log10(y)) + 1
+    (x % 10^places) == y
+end
+
+function try_ask_pipes(i)
+    max_ = length(i.elems) - 1
+    target = i.target
+    for p ∈ 1:max_
+        accum = i.elems[p]
+        #try asterisk positions - l to r, using memo to avoid recomputation
+        
+        #this is instead a division test 
+        i.target % accum != 0 && return false
+        target_ask = i.target ÷ accum
+        target_ask == i.memo[p+1] && return true #remainder are +s    
+
+        ###concat stuff 
+
+        #try concat  - this bit is broken for now
+        #deconcat test:
+        deconcat_p(i.target,accum) && return false 
+        target_pipe = i.target ÷ 10^(floor(Int64,log10(accum)) + 1)  #deconcat 
+        target_pipe == i.memo[p+1] && return true #remainder are +s
+
+        #and recurse
+        p == max_ && return false #can't recurse and out of options 
+
+        try_ask_pipes( item(target_ask, i.elems[p+1:end], i.memo[p+1:end]) ) && return true
+        try_ask_pipes( item(target_pipe, i.elems[p+1:end], i.memo[p+1:end])) && return true
+
+        # we *loop* by subtracting the next element from p!!!
+        target -= accum
+    end
+    return false
+end
+
+
+#only test the ones we can't already solve!
+remaining = setdiff!(probs, valid)
+
+valid_2 = Set{item}()
+
+for i ∈ remaining
+    try_ask_pipes(i) && push!(valid_2, i)
+end
+
+print("Pt2 = $(mapreduce(x->x.target, +, valid_2)+pt1)\n")
+
