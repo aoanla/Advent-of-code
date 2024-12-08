3817ca927e984c80dd90296719f892306e8d2cfb
aoanla <aoanla@gmail.com>
2024-12-08 17:20:23 +0000

    Julia 2024 08 pt2 explicit version. Still need to work out the |> operator properly

--- 2024/08/202408_iter.jl
+++ 2024/08/202408_iter.jl
@@ -22,12 +22,13 @@ end
 
 sites = Set{Tuple{Int32,Int32}}()
 
-values(freq_locs) |> collect |> (x -> subsets(x,Val{2}())) |> collect |> (s -> foreach(s) do 
-    d = s[2] .- s[1]
-    cs = (s[2].+d, s[1].-d)
-    foreach(x -> ((x .> (0,0)) .&& (x .<= boundary)) == (true, true) && push!(sites,x), cs )
+for v ∈ values(freq_locs)
+    for s ∈ subsets(v,Val{2}()) 
+        d = s[2] .- s[1]
+        cs = (s[2].+d, s[1].-d)
+        foreach(x -> ((x .> (0,0)) .&& (x .<= boundary)) == (true, true) && push!(sites,x), cs )
+    end
 end
-)
 
 
 print("Pt1 = $(length(sites))\n")
@@ -36,23 +37,23 @@ print("Pt1 = $(length(sites))\n")
 #δ > 0 -> floor(boundary-x / δ) 
 #δ < 0 -> floor(x-1 / δ)
 limit(δ,x) = begin
-    δ == 0 && return boundary #so big we won't pick it
-    dist = δ > 0 ? boundary - x : x - 1
-    floor(dist/δ) 
+    δ == 0 && return first(boundary) #so big we won't pick it
+    dist = δ > 0 ? first(boundary) - x : 1-x
+    dist÷δ 
 end
 
-values(freq_locs) |> collect |> (x -> subsets(x,Val{2}())) |> collect |> (s -> foreach(s) do 
-    d = (s[2] .- s[1])
-    d = d .÷ gcd(d...)
-    #"closest to zero" of each of the negative and positive integers n for s[2] .+ n.*d that are 1 or boundary -> srt, end_ TODO
-    srt = -minimum(limit.(-d,s[2]))
-    end_ = minimum(limit.(d,s[2]))
-    foreach(x->push!(sites, s[2].+(n.*d)),srt:end_)
+for v ∈ values(freq_locs)
+    for s ∈ subsets(v,Val{2}())
+
+        d = (s[2] .- s[1])
+        d = d .÷ gcd(d...)
+        #"closest to zero" of each of the negative and positive integers n for s[2] .+ n.*d that are 1 or boundary -> srt, end_ 
+        srt = minimum(limit.(d.*(-1),s[2])) .* (-1)
+        end_ = minimum(limit.(d,s[2]))
+        foreach(x->push!(sites, s[2].+(x.*d)),srt:end_)
+        end
+#    )
 end
-)
-
-
-
 
 
 print("\nPt2 = $(length(sites))\n")
