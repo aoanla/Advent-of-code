cfa1b285386f5706fa665882ba25618611a497c6
aoanla <aoanla@gmail.com>
2024-12-11 04:45:06 +0000

    Julia 2024 09 pt2 unchecked (2 stub fns)

--- 2024/09/202409.jl
+++ 2024/09/202409.jl
@@ -23,10 +23,19 @@ zero_ = UInt8('0')
 #parse input 
 file_spans = Dict{Int32,f_span}()
 empty_spans = Dict{Int32,Int32}()
+#Pt2 - this is probably a dequeue for the empty spans (we need to pop and push from the front)
+# a deque for each length of empty_span (0-9)
+empty_span_queues = [ DeQueue() for i ∈ 1-9 ]
+
+max_empty_len = 0 #fast check
 read("input") |> Base.Fix1(map, x->x-zero_) |> Base.Fix2(Base.Iterators.partition,2) |>  enumerate |> Base.Fix1((op,iter)->foldl(op,iter; init=0), (n,(i,f))->begin
-    file_spans[n]=f_span(i-1,first(f))
+    file_spans_[n]=f_span(i-1,first(f))
     if length(f) == 2
-        empty_spans[n+first(f)] = last(f)
+        empty_spans_[n+first(f)] = last(f)
+        pushlast!(empty_spans_queues[last(f)], n+first(f))  
+        if max_empty_len < last(f) 
+            max_empty_len = last(f)
+        end
     end
     n + sum(f) #next position
 end)
@@ -35,7 +44,8 @@ end)
 #print("$file_spans\n")
 #print("$empty_spans\n")
 #pt 1 algo, using spans:
-
+file_spans = deepcopy(file_spans_)
+empty_spans = deepcopy(empty_spans_)
 #exit()
 
 #poplast!(d::SortedDict{T,V}) where T,V = pop!(lastindex(d)) 
@@ -97,15 +107,15 @@ end
     #total value is file_span_id * (sum of all positions in span)
     # sum_of_all_positions from start to start+len is (2*start + len - 1)*len / 2 [Gaussian sum]
 #    tot += 
-checksum = mapreduce(+, keys(file_spans) ) do start
-    span = file_spans[start]
+checksum(file_span_dict) = mapreduce(+, keys(file_span_dict) ) do start
+    span = file_spans_dict[start]
     #print("Sum: starting at $(start) to $(start+span.len-1), mul by $(span.id)... ")
-    res = span.id*((2*start + span.len - 1)*span.len)÷2
+    span.id*((2*start + span.len - 1)*span.len)÷2
     #print("$res \n")
-    res
+    #res
 end
 
-print("$checksum")
+print("Pt1: $checksum(file_spans)\n")
 
 ##okay, so I overestimated the complexity of pt2 from looking at pt1
 # this is just iterating over my reverse_order_file_spans list, finding the 
@@ -113,3 +123,27 @@ print("$checksum")
 # it might be reasonable to maintain a max_empty_span var on parsing the list
 # so we can early-reject moving a span if no empty spans can take it. 
 # (we could also maintain a dict of vectors of empty_spans keyed by size too)
+
+
+#todo - min_index, insert_into
+
+#pt2 basically
+for fs_posn ∈ keys(file_spans_) |> collect |> (x-> sort!(x; rev=true)) 
+    fs = file_spans_[fs_posn]
+    #each span gets tried against the stuff available when it is checked.
+    fs.len > max_empty_len && continue #early exit if we never had a gap large enough
+    best_empty_idx = min_index(peekfirst.(empty_span_queues[begin:fs.len]))
+    best_empty_idx == -1  && continue #or however we indicate no valid thing found 
+    empty_posn = popfirst!(empty_span_queues)
+
+    file_spans_[empty_posn] = fs
+    delete!(file_spans_, fs_posn)
+    
+
+    if best_empty_idx > fs.len #push back remaining elem
+        insert_into(empty_span_queues[best_empty_idx-fs.len], empty_posn)
+    end
+
+end
+
+print("Pt2: $checksum(file_spans_)\n")
