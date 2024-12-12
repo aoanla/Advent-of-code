5bc70ce3f7f8cfb88388a02761558609bcd6ff0f
aoanla <aoanla@gmail.com>
2024-12-12 18:13:58 +0000

    Julia 2024 11 pt1 sketch of approach

--- 2024/11/202411.jl
+++ 2024/11/202411.jl
@@ -8,4 +8,34 @@ integer_log10(x::Int64) = begin
     end
 
 #we of course could implement an 128bit version if we needed to
-print("$(integer_log10(12345678))")
+
+
+#stones are most efficiently in a linked list
+
+
+function step!(stones, n)
+    for stone ∈ stones
+        if stone == 0
+            stone = 1
+        else
+            stonedigits = integer_log10(stone)
+            if iseven(stonedigits)
+                cutoff = 10^(stonedigits÷2)
+                stone = stone ÷ cutoff
+                next_stone = stone % cutoff
+                n += 1 
+                insert!(next_stone, after_stone) #such that we skip it this time...
+            else 
+                stone = stone * 2024 #I don't think any of the cycles can make this > 2^63...
+            end
+        end
+    end
+    n
+end 
+
+n = len(stones)
+for i ∈ 1:25 
+    n = step(stones, n)
+end
+
+print("Pt1: $n")
