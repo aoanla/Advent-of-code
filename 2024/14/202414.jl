150a531f95638d57b259ccd763e1ca1ce0ec0134
aoanla <aoanla@gmail.com>
2024-12-14 13:45:47 +0000

    Julia 2024 14 pt2 - a stab at getting ahead of the answer that didn't work

--- 2024/14/202414.jl
+++ 2024/14/202414.jl
@@ -8,16 +8,19 @@ robots = readlines("input") |> Base.Fix1(map, x->parse.(Int64,match(parser, x).c
 const lims = (101,103)
 const hlims = lims .÷ 2
 
-quads = Dict([[x,y]=>0 for x ∈ (true,false), y ∈ (true, false)])
-pt1posns = map(robots) do r
-    #lims = testlims 
-    p = mod.((r[1:2] .+ (r[3:4].*100)), lims) 
-    any(p .== hlims) && return 
-    quads[p .< hlims] += 1 
+
+function pt1(n)
+    quads = Dict([[x,y]=>0 for x ∈ (true,false), y ∈ (true, false)]) 
+    map(robots) do r
+        #lims = testlims 
+        p = mod.((r[1:2] .+ (r[3:4].*n)), lims) 
+        any(p .== hlims) && return 
+        quads[p .< hlims] += 1 
+    end
+    reduce(*, values(quads))
 end
 
-
-print("Pt1 = $(reduce(*, values(quads)))\n")
+print("Pt1 = $(pt1(100))\n")
 
 #pt2 is one of those stupid pointless questions that needs you to look at the input to work out what precisely it means by "Christmas Tree" shape.
 
@@ -30,3 +33,25 @@ print("Pt1 = $(reduce(*, values(quads)))\n")
 # a smaller picture than that?
 
 #I guess the unsatifying approach would be to output 10^4 frames and watch them [at 10frames/sec that will take 1000 seconds, or 17 or so minutes, if we need to watch all of them]
+
+# the other argument may be that pt1 is supposed to be relevant - the "detecting numbers in quadrants" suggests that maybe the picture *is* going to be in one quadrant.
+#  -> maybe we need solutions with a significant imbalance of robots in one quadrant relative to all the others (because I assume that's what "most of the robots form a picture" means)
+
+# the product of all the quads is maximised by an equal distribution (that is, if N total, max(a*b*c*(N-a-b-c)) is when a=b=c=N/4 -> N^4/256) if N >> 4
+# and minimised by an unequal distribution with all in one quad (as that's zero) or a few in 3 and most in one (~N)
+
+function pt2()
+    minn = 0
+    minval = 10000000000000
+    for n ∈ 1:prod(lims)
+        val = pt1(n)
+        if val < minval
+            minn = n 
+            minval =val
+        end 
+    end
+    minn
+end
+
+print("Pt2: $(pt2())\n")
+
