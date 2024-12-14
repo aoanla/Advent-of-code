235942ddeee3abac8a3267b6e7ab24713095af87
aoanla <aoanla@gmail.com>
2024-12-14 13:29:38 +0000

    Julia 2024 14 pt2 moaning about poor spec of problem

--- 2024/14/202414.jl
+++ 2024/14/202414.jl
@@ -20,3 +20,13 @@ end
 print("Pt1 = $(reduce(*, values(quads)))\n")
 
 #pt2 is one of those stupid pointless questions that needs you to look at the input to work out what precisely it means by "Christmas Tree" shape.
+
+#some thoughts:
+# height, width are coprime (in fact, they're prime-prime!). So, all of the robots will visit *every* point over lcm(height,width) = height*width steps
+# so there's "only" ~10^4 possible configurations.
+
+#Problem
+#we don't know what it means by "picture of a Christmas tree". Is this a picture across the entire grid? A picture in one quadrant (maybe we just look for "all robots in one quad")
+# a smaller picture than that?
+
+#I guess the unsatifying approach would be to output 10^4 frames and watch them [at 10frames/sec that will take 1000 seconds, or 17 or so minutes, if we need to watch all of them]
