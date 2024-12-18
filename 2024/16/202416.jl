ec40c97887b2d5d5bd89ac177eb62ee21bb0cd1b
aoanla <aoanla@gmail.com>
2024-12-18 21:26:21 +0000

    Julia 2024 16 pt2 some thinking about how to represent my path segment reconstruction for all segs

--- 2024/16/202416.jl
+++ 2024/16/202416.jl
@@ -38,10 +38,13 @@ possibles = Set([(0,1), (1,0), (0,-1), (-1,0) ]);
 
 
 #this reconstructs *a* path (if there are multiple, it reconstructs the one we found first)
+#luckily, our E has only one possible approach [from the West], so we don't need to worry about reconstructing multiple cursors 
+#we do need to worry about what we're storing - the original version of this stored breadcrumbs of cells, whilst here we need to store path_segments themselves
 function reconstruct_path(prev, cursor)
-    totalpath = [cursor]
-    while cursor in keys(prev)
-        cursor = prev[cursor]
+    totalpath = Set([cursor]) #this isn't true now as when we start, we don't have a *segment* just a starting point 
+    while cursor ∈ keys(prev)
+        cursor_set = prev[cursor]
+
         pushfirst!(totalpath, cursor)
     end
     totalpath
@@ -91,14 +94,12 @@ g = Node(g_loc, (0,0)) #we actually don't care about the dir for g
 #A✴ algorithm from a 2023 puzzle so I don't need to sort it out properly again
 function A✴(s::Node, g::Node, grid)  #more than one end point, since we don't care about orientation when you arrive
 
-    #prev = Dict{Node, Node}() #dictionary of previous points
+    prev = Dict{Node, Set{Tuple{Node,Cost}}}() #dictionary of previous points, as sets
 
-    #state space is [dir, amount] for each [location] - I really want to do this with a sparse DefaultDict or something but those seem slow
+    #state space is dir for each location, as part of Node
     #in this case, the state space is much smaller, since it only contains nodes - this needs to be a map
     goalscore = Dict{Node, Int64}()  #cost s -> cell
     goalscore[s] = 0 #zero cost to not move at all!
-
-
 
     #not having this around seems to make it slow (even though we only use it to look up score, which is already stored in the PQ)
     fscore = Dict{Node, Int64}() 
@@ -125,8 +126,8 @@ function A✴(s::Node, g::Node, grid)  #more than one end point, since we don't 
             for (cand,cost) ∈ accessible(cursor, grid, s_e)
                 
                 trialgoalscore = get(goalscore,cursor,typemax(1)) + cost;
-                if trialgoalscore < get(goalscore,cand,typemax(1))
-                    #prev[cand] = cursor 
+                if trialgoalscore <= get(goalscore,cand,typemax(1))
+                    prev[cand] = get(prev, cand, Set{Tuple{Node,Cost}}()) ∪ (cursor,cost)  #a cell can have several previous candidates, and we do need to store the costs
                     goalscore[cand] = trialgoalscore
                 
                     fscore[cand] = trialgoalscore + h(cand, g)
@@ -141,3 +142,11 @@ function A✴(s::Node, g::Node, grid)  #more than one end point, since we don't 
 end
 
 print("$(A✴(s,g, grid))\n")
+
+
+#Pt2 thoughts - Dijkstra, at least, *does* find multiple paths (because it identifies the distance to all possible nodes
+#                                                                so you just need to trace back all "equally good" distances at each point)
+# I think A✴ can also do this, if we don't just stop as soon as we find the goal?
+
+#the number of tiles on any path segment is the cost of that path segment % 1000 (unless it's longer than 1000 cells long, which I doubt happens in a 139x139 maze)
+#so, if we can find all the path segments on an optimal path, the number of tiles is just mapreduce(x->x%1000, +, cost_of_segments)
