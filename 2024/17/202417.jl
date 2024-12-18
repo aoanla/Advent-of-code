cab354a2b2da8f44884a815834d2ab47a68eb6eb
aoanla <aoanla@gmail.com>
2024-12-18 10:57:56 +0000

    Julia 2024 17 pt1 partial pt2 (reversible operations!)

--- 2024/17/202417.jl
+++ 2024/17/202417.jl
@@ -2,40 +2,79 @@
 #except with most of the operations being shr s, you expect it's going to have a limited
 #set of outputs 
 
-register = [0,0,0]
+#ip = item 4
+register::Vector{Int128} = [729,0,0, 1]
 
-instructions = []
-ip = 1
-output = ""
+instructions = [0,1,5,4,3,0]
 
-combo(op) = op < 4 ? op : register[op-3]
 
-adv(op) = global register[1] >>= combo(op)
-bxl(op) = global register[2] ⊻= op
-bst(op) = global register[2] = combo(op) & 7
-jnz(op) = begin
-    if register[1] != 0 
-        global ip = op-2  #allow for auto movement after
+input = readlines("input")
+for i ∈ 1:3
+    register[i] = parse(Int128, input[i][12:end])
+end
+instructions = parse.(Int, split(input[5][9:end], ','))
+
+print("Initial state:\n Registers: $register\nProgram: $instructions\n")
+
+combo(op, reg) = op < 4 ? op : reg[op-3]
+
+adv(op, reg) = reg[1] >>= combo(op, reg)
+bxl(op, reg) = reg[2] ⊻= op
+bst(op, reg) = reg[2] = combo(op, reg) & 7
+jnz(op, reg) = begin
+    if reg[1] != 0 
+        reg[4] = op-1  #allow for auto movement after; this is "op-2" but plus 1 because Julia indexes at 1 not 0
     end
 end
-bxc(op) = global register[2] ⊻= register[3]
-out(op) = print("$(combo(op) & 7)")
-bdv(op) = global register[2] = register[1] >> combo(op)
-cdv(op) = global register[3] = register[1] >> combo(op)
+bxc(_, reg) = reg[2] ⊻= reg[3]
+out(op, reg) = print("$(combo(op, reg) & 7),")
+bdv(op, reg) = global reg[2] = reg[1] >> combo(op, reg)
+cdv(op, reg) = global reg[3] = reg[1] >> combo(op, reg)
 
 decoder = Dict([0=>adv, 1=>bxl, 2=>bst, 3=>jnz, 4=>bxc, 5=>out, 6=>bdv, 7=>cdv])
 
-function op(opcode, operand)
-    decoder[opcode](operand)
-    ip+=2
+function op(opcode, operand, reg)
+    decoder[opcode](operand, reg)
+    reg[4]+=2
 end
 
 
-lastindex = length(instructions)
+function runtape(instructions, register)
+    lastindex = length(instructions)
+    while register[4] < lastindex
+        op(instructions[register[4]], instructions[register[4]+1], register)
+    end 
+    print("\n")
+end
 
-while ip < lastindex
-    op(instructions[ip], instructions[ip+1])
-end 
-
+runtape(instructions, register)
 
 #testoutput should be: 4,6,3,5,6,3,5,2,1,0
+
+#Pt2 
+
+#so, this is a turing machine in a turing machine - and this explains all those "hidden" shift-rights 
+# we're reading parts of A each loop - so we need to work out what math is done to the part of A we read each time to work out how to encode the 
+# sequence in it. The output sequence is 16 digits => 16*3 = at least 48 bits needed in A, assuming only shifts right by a max of 3
+
+#okay so this loop (and all loops like it) establishes a recurrence relation between sets of triple bits in A 
+# we can resolve this starting from the far end (because the recurrence is between the current and *higher* bits, and there are no higher bits for the far end)
+# and then repeatedly unpick 
+
+function decode(instructions) 
+    value = 0
+    for nibble ∈ reverse(instructions)
+
+        tmp = ( nibble ⊻ (value >> B) ) & 7 #the issue here is knowing the value of B here so we do need to actually run the code backwards
+        value <<= 3
+        value += tmp
+    end
+    value
+end 
+
+value = decode(instructions)
+
+print("Result: $value\n")
+#test 
+print("Test:")
+runtape(instructions, [value, 0, 0, 1])
