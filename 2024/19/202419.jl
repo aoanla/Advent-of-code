7aec5b41edf63c5045360e737915385ba353869b
aoanla <aoanla@gmail.com>
2024-12-19 09:33:25 +0000

    Julia 2024 19 thoughts

---
+++ 2024/19/202419.jl
@@ -0,0 +1,4 @@
+#this looks like subsequence matching [with backtracking] like soln for 17pt2. 
+#in this case, we probably need to memoise to make it tractable.
+
+#note - my input sequence, at least, has 4 of 5 colours as single letters (just missing a 'w'), so all sequences with no 'w' are trivially possible.
