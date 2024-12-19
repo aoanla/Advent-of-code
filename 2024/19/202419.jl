43dd4f85b8fa882deda3839c5702ff8d770d5f6b
aoanla <aoanla@gmail.com>
2024-12-19 19:10:19 +0000

    Julia 2024 19 more brief note expansion

--- 2024/19/202419.jl
+++ 2024/19/202419.jl
@@ -2,3 +2,8 @@
 #in this case, we probably need to memoise to make it tractable.
 
 #note - my input sequence, at least, has 4 of 5 colours as single letters (just missing a 'w'), so all sequences with no 'w' are trivially possible.
+
+#we probably want to order the matchable subsequences by length, and by first letter?, and try to greedily match the longer ones first (and then backtrack if the
+# remaining parts of the string can't match things)
+
+#we can memoise successful combinations of subsequences (and mark them as "not original" in the dictionary)
