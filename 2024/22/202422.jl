d0dda0b23715f24372305c511c4c31e55f25b997
aoanla <aoanla@gmail.com>
2024-12-22 12:02:52 +0000

    Julia 2024 22 pt2 (brute force, basically)

--- 2024/22/202422.jl
+++ 2024/22/202422.jl
@@ -15,3 +15,41 @@ print("pt1: $pt1\n")
 #okay so part2 isn't what I was dreading - it's a maximisation problem on sequence matching 
 #intuition suggests that we need to combine "popular subsequences" with "subsequences followed by big numbers".
 # we have about 1.5k sequences, each with 2k differences
+
+# I do worry that we're supposed to do something clever knowing that we only care about the deltas in the lowest 4 bits of each sequence.
+# since the low bits of "pure" xorshift PRNGs are known weak.
+#ah, actually we do know this - the left-shifted xors only touch the upper bits - because they're all zero below 64 | 2048. 
+#                               so each time the *lower* bits follow a pattern of x ⊻ (x' >> 5) where x' is x ⊻ (x << 6)
+#                                                                                 x ⊻ (x >> 5 ⊻ x << 1)
+# not sure if this is actually helpful, but it does mean we can expect low entropy and a lot of repeating patterns in our sequences once processed 
+
+encode(x) = foldl((n,i)->(n<<6)+i, x; init=0)
+seq_totals = fill(0, 9586981) #the sequences, encoded a little lossily as 4 x 5bit values
+
+for seed ∈ parse.(Int64, readlines("input"))
+    #repeat buffer, true if we've not met this sequence before for this seed 
+    buffer = fill(true,9586981)
+    oldn = seed
+    seq = Vector{Int64}()
+    for i ∈ 1:3 #fill first seq value - nothing until we have this many diffs
+        n = xorshift(oldn)
+        x = (n % 10) - (oldn % 10) 
+        push!(seq, x+18)
+        oldn = n
+    end
+    for i ∈ 4:2000
+        n = xorshift(oldn)
+        x = (n % 10) - (oldn % 10) 
+        seq = push!(seq, x+18)
+        index = encode(seq) + 1
+        if buffer[index] 
+            global seq_totals[index] += (n%10) #add the increment for this seed for this sequence
+            buffer[index] = false
+        end
+        oldn = n
+        popfirst!(seq)
+    end
+end
+
+sort!(seq_totals)
+print("$(last(seq_totals))\n")
