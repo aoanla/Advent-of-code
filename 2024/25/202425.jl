c3df63ae3dee08311fbb2882b3a17536fe1964f1
aoanla <aoanla@gmail.com>
2024-12-25 21:22:45 +0000

    Julia 2024 25 (pt1)

---
+++ 2024/25/202425.jl
@@ -0,0 +1,14 @@
+
+fits((p1,p2)) = all((p1.+p2).<6)
+
+function parse_input(input_)  #yes, there's a nicer way to do this by reading the known chunk size and then parsing it as one by rotating it, but meh
+    blocks = readlines(input_) |> Base.Fix2(Iterators.partition, 8) |> Base.Fix1(map, x->reduce(hcat, collect.(collect(x)[1:7])))
+    locks = filter(x->x[1]=='#', blocks) |> Base.Fix1(map, x->sum(==('#'), x, dims=2).-1)
+    keys_ = filter(x->x[1]!='#', blocks) |> Base.Fix1(map, x->sum(==('#'), x, dims=2).-1)
+    (keys_, locks)
+end 
+(k, l) = parse_input("input")
+
+#print("keys = $k \n locks = $l \n")
+res = mapreduce(x->fits(x),  +  , Iterators.product(k,l))
+print("res: $res\n")
